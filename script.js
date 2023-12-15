172954dad9a82f8180ca0f8119ab2fb6ee28dd94
ANILKUMARRegidi <129140166+ANILKUMARRegidi@users.noreply.github.com>
2023-12-15 11:12:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+   let count = 0;
+	for(let char of str)
+		{
+			count = (count[char]||0) + 1;
+		}
+	for(let char of str)
+		{
+			if count(char) === 1)
+			{
+				return char;
+			}
+		}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
